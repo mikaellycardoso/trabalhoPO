79c98d327aeb8d27a40575aba52c7408596ceaef
Mikaelly Cardoso <mikaellycardoso7@outlook.com>
2025-11-23 20:07:38 -0300

    Update NoAVL.java

--- src/trabalhoPO/NoAVL.java
+++ src/trabalhoPO/NoAVL.java
@@ -1,49 +1,55 @@
 package trabalhoPO;
 
+import java.util.ArrayList;
+
 public class NoAVL {
 
-    private Reserva item;
+    private ArrayList<Reserva> listaItens;
+    private int altura;
     private NoAVL esq;
     private NoAVL dir;
-    private int fatorBalanceamento;
 
     public NoAVL(Reserva item) {
-        this.item = item;
+        this.listaItens = new ArrayList<>();
+        this.listaItens.add(item);
+        this.altura = 1; 
         this.esq = null;
         this.dir = null;
-        this.fatorBalanceamento = 0;
     }
 
-    public Reserva getItem() {
-        return item;
+    public Reserva getChave() {
+        return listaItens.get(0);
+    }
+
+    public ArrayList<Reserva> getListaItens() {
+        return listaItens;
+    }
+
+    public void adicionarItem(Reserva item) {
+        this.listaItens.add(item);
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
     }
 
     public NoAVL getEsq() {
         return esq;
     }
 
-    public NoAVL getDir() {
-        return dir;
-    }
-
-    public int getFatorBalanceamento() {
-        return fatorBalanceamento;
-    }
-
-    public void setItem(Reserva item) {
-        this.item = item;
-    }
-
     public void setEsq(NoAVL esq) {
         this.esq = esq;
+    }
+
+    public NoAVL getDir() {
+        return dir;
     }
 
     public void setDir(NoAVL dir) {
         this.dir = dir;
     }
-
-    public void setFatorBalanceamento(int fatorBalanceamento) {
-        this.fatorBalanceamento = fatorBalanceamento;
-    }
-
 }
